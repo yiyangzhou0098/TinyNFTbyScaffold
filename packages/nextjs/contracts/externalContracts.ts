d974aa132e06b5c4315cc5decab3a47325b51222
zyy0098 <770823669@qq.com>
2024-12-07 21:57:29 -0500

    debug deployment

--- packages/nextjs/contracts/externalContracts.ts
+++ packages/nextjs/contracts/externalContracts.ts
@@ -3,13 +3,13 @@ import MyNFTCollectionJSON from "./MyNFTCollection.json";
 // const MyNFTCollectionABI = MyNFTCollectionJSON.abi; 
 
 const externalContracts = {
-    31337: {
-        MyNFTCollection: {
-            // for test
-            address: "", // Replace with the deployed contract address when known
-            abi: MyNFTCollectionJSON.abi, // Ensure this is the flat ABI array
-        },
-      },
+    // 31337: {
+    //     MyNFTCollection: {
+    //         // for test
+    //         address: "", // Replace with the deployed contract address when known
+    //         abi: MyNFTCollectionJSON.abi, // Ensure this is the flat ABI array
+    //     },
+    //   },
 } as const;
 
 
